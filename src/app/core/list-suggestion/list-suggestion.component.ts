5a317a22a75b87653304b9429a4fe3dd07038afd
Badr Klila <klilabader2@gmail.com>
2025-11-30 21:39:29 +0100

    fixed the likes and the favorite in the Workshop n°1-Manipulation des Composants

--- src/app/core/list-suggestion/list-suggestion.component.ts
+++ src/app/core/list-suggestion/list-suggestion.component.ts
@@ -7,6 +7,9 @@ import {Suggestion} from '../../models/suggestion';
   styleUrl: './list-suggestion.component.css'
 })
 export class ListSuggestionComponent {
+  searchTerm: string = '';
+  favourites : Suggestion[] = []
+
   suggestions: Suggestion[] = [
     {
       id: 1,
@@ -14,7 +17,8 @@ export class ListSuggestionComponent {
       description: 'Suggestion pour organiser une journée de team building pour renforcer les liens entre les membres de l\'équipe.',
       category: 'Événements',
       date: new Date('2025-01-20'),
-      status: 'acceptee'
+      status: 'acceptee',
+      likes:0
     },
     {
       id: 2,
@@ -22,7 +26,8 @@ export class ListSuggestionComponent {
       description: 'Proposition pour améliorer la gestion des réservations en ligne avec un système de confirmation automatique.',
       category: 'Technologie',
       date: new Date('2025-01-15'),
-      status: 'refusee'
+      status: 'refusee',
+      likes:0
     },
     {
       id: 3,
@@ -30,7 +35,8 @@ export class ListSuggestionComponent {
       description: 'Mise en place d\'un programme de récompenses pour motiver les employés et reconnaître leurs efforts.',
       category: 'Ressources Humaines',
       date: new Date('2025-01-25'),
-      status: 'refusee'
+      status: 'refusee',
+      likes:0
     },
     {
       id: 4,
@@ -38,7 +44,8 @@ export class ListSuggestionComponent {
       description: 'Refonte complète de l\'interface utilisateur pour une meilleure expérience utilisateur.',
       category: 'Technologie',
       date: new Date('2025-01-30'),
-      status: 'en_attente'
+      status: 'en_attente',
+      likes:0
     },
     {
       id: 5,
@@ -46,20 +53,28 @@ export class ListSuggestionComponent {
       description: 'Organisation d\'une formation sur les bonnes pratiques de sécurité informatique pour tous les employés.',
       category: 'Formation',
       date: new Date('2025-02-05'),
-      status: 'acceptee'
+      status: 'acceptee',
+      likes:0
     }
     ];
 
-  likeCount: number = 0;
+  filteredSuggestions: Suggestion[] = [...this.suggestions];
 
-  searchTerm: string = '';
 
-  addLike(): void {
-    this.likeCount++;
+
+  addLike(id:number): void {
+    let suggestion: Suggestion | undefined  ;
+     suggestion = this.suggestions.find(sugg => sugg.id === id )
+      if(suggestion) {
+        suggestion.likes = suggestion.likes + 1;
+      }
+      else {
+        console.warn(`Suggestion with id ${id} not found`);
+      }
 
   }
 
-  filteredSuggestions: Suggestion[] = [...this.suggestions];
+
 
   filterItems(): void {
     if (!this.searchTerm) {
@@ -77,4 +92,19 @@ export class ListSuggestionComponent {
 
 
 }
+  addToFavourite  = (id:number)=>{
+
+    let suggestion: Suggestion | undefined  ;
+    suggestion = this.suggestions.find(sugg => sugg.id === id )
+    if(suggestion) {
+     this.favourites.push(suggestion);
+    alert("added")
+    }
+    else {
+      console.warn(`Suggestion with id ${id} not found`);
+    }
+
+
+  }
+
 }
